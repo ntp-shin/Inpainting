5099f1094bea5103c092780e6bf7c44089dc983e
Tan Phat <tanphatnguyen2002@gmail.com>
2024-07-27 22:08:34 +0700

    1. Load project

---
+++ README.md
@@ -0,0 +1,86 @@
+<h1 align="center">
+<p>Inpainter
+</h1>
+<h3 align="center">
+<p>A python GUI application to inpaint images.
+</h3>
+
+*Inpainting* is a set of image processing algorithms where damaged, missing or unwanted parts of an image are filled in using the neighbouring pixels. It can also be used to 
+remove forground objects. This is a GUI application that helps you do just that. It has an interactive and user-friendly GUI for marking the regions of the images. 
+
+<p align="center">
+ <img alt="cover" src="https://github.com/Zedd1558/Image-Inpainter/blob/master/demo/cover.jpg" height="50%" width="50%">
+</p>
+
+
+### Implementation
+The frontend GUI is developed using PyQt. The backend inpainting operations are done using *OpenCV* library. Currently, *OpenCV* provides two algorithms for inpainting which are-
+* cv2.INPAINT_TELEA: An image inpainting technique based on the fast marching method (Telea, 2004)
+* cv2.INPAINT_NS: Navier-stokes, Fluid dynamics, and image and video inpainting (Bertalmío et al., 2001)
+
+I've mentioned how you can quickly incorporate other inpainting algorithms with this GUI applications down below. Later on, I'll try to incorporate recent deep learning methods that perform way better than these classical image processing algorithms. 
+
+<h4 align="center">
+<p>let's see an exmaple
+</h4>
+<p align="center">
+ <img alt="editing" src="https://github.com/Zedd1558/Image-Inpainter/blob/master/demo/editpage.jpg">
+</p>
+<h4 align="center">
+<p>removes text quite well!
+</h4>
+
+### Required libraries
+PyQt, Numpy, OpenCV3, qimage2ndarray
+
+### How to run
+open up console in the project directory and enter this 
+```
+python inpainter.py
+```
+<p align="center">
+ <img alt="editing" src="https://github.com/Zedd1558/Image-Inpainter/blob/master/demo/inpaint_demo2.gif">
+</p>
+
+
+### Here's how you can quickly incorporate other inpainting methods
+Let's say you want to add the inpainting algorithm **Deepfill**. Here's how you can do it:
+
+**1.** Open up *editpage.py*. Go to the function *setupUi()* of class *Editpage*. Add the following line at the very end of this function.
+```python
+self.addInpaintingMethod("Deepfill")
+```
+This will add your new method's name in the *dropdown selection list* of the GUI editing page.
+
+**2.** Now, in *backend.py* you can add a function that will call your inpainting algorithm.
+```python
+def inpaint_deepfill(image, mask):
+    # call your custom algorithm for inpainting here and pass your image and mask to your algorithm
+    # return your output image with format numpy ndarray, for now I am just returning the input image
+    return image    
+```
+**3.** Last thing you need to do is call your inpainting method in *backend.py* from *editor.py*. Go to the function *inpaint()* of *editor.py*. Add an *elif condition* which checks `self._method` by your method's name and calls the corresponding inpainting method in *backend.py*
+```python
+    def inpaint(self):
+        img = np.array(self._current_image)                   
+        mask = rgb_view(self._mask)
+      
+        if self._method == "Navier-Stokes":
+            output_rgb = backend.inpaint_cv2(img, mask,method="ns")
+            
+        elif self._method == "Telea":
+            output_rgb = backend.inpaint_cv2(img, mask,method="telea")
+         
+        elif self._method == "Deepfill":                                  # add these lines 
+            output_rgb = backend.inpaint_deepfill(img, mask)              # to call your inpainting algorithm
+            
+        else:
+            raise Exception("this inpainting method is not recognized!")
+        output = array2qimage(output_rgb)
+```
+
+
+
+### Contribute
+Feel free to fork the project and contribute. You can incorporate recent deep learning methods, make the GUI more easy to use, include relevant photo editing features. 
+
---
+++ editor.py
@@ -0,0 +1,228 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QSizePolicy, QGraphicsView, QGraphicsScene, QSlider, QFileDialog, QShortcut
+from PyQt5.QtCore import Qt, QRectF, pyqtSignal, QT_VERSION_STR, QPoint
+from PyQt5.QtGui import QImage, QPixmap, QPainterPath, QPen, QPainter, QActionEvent, QKeySequence
+import numpy as np
+import backend
+from qimage2ndarray import rgb_view, array2qimage
+import cv2
+
+class Editor(QtWidgets.QGraphicsView):
+
+    def __init__(self, scene = None, parent = None, slider = None):
+        super(Editor, self).__init__(parent)
+        self._zoom = 0
+        self._empty = True
+        if scene is not None:
+            self._scene = scene
+        else:
+            self._scene = QtWidgets.QGraphicsScene(self)
+        
+        self._photo = QtWidgets.QGraphicsPixmapItem()
+        self._scene.addItem(self._photo)
+        self.setScene(self._scene)
+        
+        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.setFrameShape(QtWidgets.QFrame.NoFrame)
+
+        self.drawMode = True
+        self.drawing = False
+        self.brushColor = "red"
+        self.brushSlider = slider
+        self.lastPoint = QPoint()
+        self.color_index = {"red":0, "green":1, "blue":2}
+
+        self._method = "Telea"
+        self._mask = None
+        self._current_image = None
+        self._unmarkedImage = None
+        self.set_mask = False
+
+        self.inpaintSc = QShortcut(QKeySequence('Ctrl+I'), self)
+        self.inpaintSc.activated.connect(self.inpaint)
+        self.saveSc = QShortcut(QKeySequence('Ctrl+S'), self)
+        self.saveSc.activated.connect(self.save)
+        self.resetSc = QShortcut(QKeySequence('Ctrl+Z'), self)
+        self.resetSc.activated.connect(self.reset)        
+
+    def setInpaintingMethod(self, method):
+        self._method = method
+
+    def hasPhoto(self):
+        return not self._empty
+
+    def fit(self, scale=True):
+        rect = QtCore.QRectF(self._photo.pixmap().rect())
+        if not rect.isNull():
+            self.setSceneRect(rect)
+            if self.hasPhoto():
+                unity = self.transform().mapRect(QtCore.QRectF(0, 0, 1, 1))
+                self.scale(1 / unity.width(), 1 / unity.height())
+                viewrect = self.viewport().rect()
+                scenerect = self.transform().mapRect(rect)
+                factor = min(viewrect.width() / scenerect.width(),
+                             viewrect.height() / scenerect.height())
+                self.scale(factor, factor)
+            self._zoom = 0
+
+
+    def setMask(self):
+        self._mask = QImage(self._photo.pixmap().size(), QImage.Format_RGB32)
+        self._mask.fill(Qt.black)
+
+    def setPhoto(self, pixmap=None):
+        if pixmap.height() > 720 or pixmap.width() > 1280:
+            pixmap = pixmap.scaled(1280, 720, Qt.KeepAspectRatio)
+        self._zoom = 0
+        self._unmarkedImage = QPixmap(pixmap)
+        self._current_image = rgb_view(pixmap.toImage())
+        if pixmap and not pixmap.isNull():
+            self._empty = False
+            self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+            self._photo.setPixmap(pixmap)
+        else:
+            self._empty = True
+            self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+            self._photo.setPixmap(QtGui.QPixmap())  
+        self.setMask()
+
+
+    def wheelEvent(self, event):
+
+        if self.hasPhoto():
+            if event.angleDelta().y() > 0:
+                factor = 1.25
+                self._zoom += 1
+            else:
+                factor = 0.8
+                self._zoom -= 1
+            if self._zoom > 0:
+                self.scale(factor, factor)
+            elif self._zoom == 0:
+                self.fit()
+            else:
+                self._zoom = 0
+        super(Editor, self).wheelEvent(event)            
+    
+    
+    def mouseMoveEvent(self, event):
+
+        if(event.buttons() and Qt.LeftButton) and self.drawing:
+            if self.set_mask:
+                self.setMask()
+                self.set_mask = False
+            painter = QPainter (self._mask)
+            if not painter.isActive():
+                painter.begin(self)
+            painter.setRenderHint(QPainter.Antialiasing, True)
+            if self.drawMode:
+                painter.setPen(QPen(Qt.white, self.brushSlider.value(), Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+            else:
+                painter.setPen(QPen(Qt.black, self.brushSlider.value(), Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+            painter.drawLine(self.lastPoint, self.mapToScene(event.pos()))
+            painter.end()
+            self.lastPoint = self.mapToScene(event.pos())
+            img = np.array(self._current_image, dtype = np.float32)
+            mask = rgb_view(self._mask)
+            channel = self.color_index[self.brushColor]
+            ind = np.all(mask != [0,0,0], axis=-1)
+            color = np.array([50,50,50], dtype = np.float32)
+            color[channel] = 250
+            img[ind] = img[ind]*0.45 + color*0.55
+            self._photo.setPixmap(QPixmap(array2qimage(img))) #
+
+        super(Editor, self).mouseMoveEvent(event)        
+
+
+    def mousePressEvent(self, event):
+
+        if event.button() == Qt.LeftButton:            
+            self.drawing = True
+            self.lastPoint = self.mapToScene(event.pos())
+
+
+    def mouseReleaseEvent(self, event):
+
+        if self.drawMode:
+            if event.button() == Qt.LeftButton:
+                self.drawing = False
+        super(Editor, self).mouseReleaseEvent(event)    
+
+
+    def inpaint(self):
+
+        img = np.array(self._current_image)              
+        mask = rgb_view(self._mask)
+
+        if self._method == "Navier-Stokes":
+            output_rgb = backend.inpaint_cv2(img, mask,method="ns")
+
+        elif self._method == "Telea":
+            output_rgb = backend.inpaint_cv2(img, mask,method="telea") 
+
+        elif self._method == "Deepfill":                        
+            output_rgb = backend.inpaint_deepfill(img, mask)        
+        
+        #############################################
+        # add calls to your inpainting methods here #
+        #############################################
+
+        else:
+            raise Exception("this inpainting method is not recognized!")
+
+        output = array2qimage(output_rgb)
+        self.set_mask = True
+        self.setMask()
+        self._current_image = output_rgb
+        self._photo.setPixmap(QPixmap(output))
+
+
+    def save(self):
+        try:
+            image_path, _ = QFileDialog.getSaveFileName() 
+            img = self._photo.pixmap().toImage()
+            if not image_path.endswith(".png"):
+                image_path += ".png"
+            img.save(image_path, "PNG")
+        except:
+            pass
+
+    def reset(self):
+        self._photo.setPixmap(self._unmarkedImage) 
+        self.set_mask = True
+        self.setMask()
+        self._current_image = rgb_view(self._unmarkedImage.toImage())      
+
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.viewer = Editor(self)
+
+        self.btnLoad = QtWidgets.QToolButton(self)
+        self.btnLoad.setText('Load image')
+        self.btnLoad.clicked.connect(self.loadImage)
+
+        VBlayout = QtWidgets.QVBoxLayout(self)
+        VBlayout.addWidget(self.viewer)
+        HBlayout = QtWidgets.QHBoxLayout()
+        HBlayout.setAlignment(QtCore.Qt.AlignLeft)
+        HBlayout.addWidget(self.btnLoad)
+        VBlayout.addLayout(HBlayout)
+
+    def loadImage(self):
+        fileName, dummy = QFileDialog.getOpenFileName(self, "Open image file.")
+        self.viewer.setPhoto(QPixmap(fileName))
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = Window()
+    window.setGeometry(500, 300, 800, 600)
+    window.show()
+    sys.exit(app.exec_())
+
+
